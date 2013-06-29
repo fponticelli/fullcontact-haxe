4424bc6fa29bc5343a69e3d49a88416b9090fd29
Franco Ponticelli <franco.ponticelli@gmail.com>
2013-06-29 12:49:44 -0600

    first working test

--- src/fullcontact/FullContactAPI.hx
+++ src/fullcontact/FullContactAPI.hx
@@ -1,10 +1,68 @@
 package fullcontact;
 
+import thx.react.Promise;
+
 class FullContactAPI
 {
+	public static var PROTOCOL = "https";
+	public static var HOST = "api.fullcontact.com";
 	public var apiKey : String;
 	public function new(apiKey : String)
 	{
 		this.apiKey = apiKey;
 	}
+
+	public function lookupPersonByEmail(email : String, ?options : LookupPersonByEmailOptions)
+	{
+		var qs = normalizeLookupParams(merge(options, { apiKey : apiKey, email : email })),
+			format = extractFormat(qs);
+		return execute(true, "v2", "person", format, qs);
+	}
+
+	public function lookupPersonByEmailMD5(emailMD5 : String, ?options : LookupPersonByEmailOptions)
+	{
+		var qs = normalizeLookupParams(merge(options, { apiKey : apiKey, emailMD5 : emailMD5 })),
+			format = extractFormat(qs);
+		return execute(true, "v2", "person", format, qs);
+	}
+
+	function execute(use_get : Bool, version : String, uri : String, format : String, qs : {})
+	{
+		var http = new haxe.Http('$PROTOCOL://$HOST/$version/$uri${null==format?"":"."+format}'),
+			deferred = new Deferred();
+		if(null != qs)
+			Reflect.fields(qs).map(function(key) http.setParameter(key, Reflect.field(qs, key)));
+		http.onData = function(data) deferred.resolve(data);
+		http.onError = function(err) deferred.reject(err);
+		http.request(!use_get);
+		return deferred.promise;
+	}
+
+	static function normalizeLookupParams(opt : Dynamic)
+	{
+		if(opt.queue)
+			opt.queue = 1;
+		return opt;
+	}
+
+	static function extractFormat(opt : {})
+	{
+		var format = Reflect.field(opt, "format");
+		if(null == format)
+			format = "json";
+		else
+			Reflect.deleteField(opt, "format");
+		return format;
+	}
+
+	static function merge(src : {}, ?dst : {})
+	{
+		if(null == dst) dst = {};
+		Reflect.fields(src).map(function(key) Reflect.setField(dst, key, Reflect.field(src, key)));
+		return dst;
+	}
+}
+
+typedef LookupPersonByEmailOptions = {
+	?queue : Bool
 }
--- test/Test.hx
+++ test/Test.hx
@@ -1,13 +1,19 @@
 import fullcontact.FullContactAPI;
 import utest.Assert;
+import thx.react.Promise;
+using Test;
 
 class Test
 {
+	static var TIMEOUT = 4000;
 	public function new() { }
 
 	public function testApi()
 	{
 		var api = new FullContactAPI(Config.FULLCONTACT_APIKEY);
+		api.lookupPersonByEmail("franco.ponticelli@gmail.com").assertPromise(function(data) {
+			Assert.stringContains('"requestId', data);
+		});
 	}
 
 	static function main()
@@ -16,5 +22,20 @@ class Test
 			report = utest.ui.Report.create(runner);
 		runner.addCase(new Test());
 		runner.run();
+	}
+
+	static function assertPromise<T>(p : Promise<T -> Void>, handler : T -> Void)
+	{
+		var async = Assert.createAsync(function() { }, TIMEOUT);
+		p.then(
+			function(a) {
+				handler(a);
+				async();
+			},
+			function(err : Dynamic) {
+				Assert.fail(Std.string(err));
+				async();
+			}
+		);
 	}
 }
